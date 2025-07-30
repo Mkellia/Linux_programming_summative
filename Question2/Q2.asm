81aa9375f686ed4881cb5adb8aee4a03db49a490
Mkellia <k.muzira@alustudent.com>
2025-07-30 10:42:36 +0200

    Asm

--- Question2/Q2.asm
+++ Question2/Q2.asm
@@ -1,119 +1,137 @@
-section .data
-    filename db "names.txt", 0
-    buffer_size equ 4096
-    newline db 10
-    msg db "Line count: ", 0
+; Program: Count lines (names) in a file
+; File: count_names.asm
+; Platform: Linux x86-64
+; Assemble: nasm -f elf64 count_names.asm -o count_names.o
+; Link: ld count_names.o -o count_names
+
+SECTION .data
+    filename db "names.txt", 0           ; file name
+    buffer_size equ 4096                 ; size of buffer for reading
+    newline db 10                        ; newline character '\n'
+    msg db "Number of lines (names): ", 0
     msg_len equ $ - msg
-    num_buf db 0, 0, 0, 0, 0, 0, 0, 0, 0, 0     ; for printing int
 
-section .bss
-    buffer resb buffer_size
-    line_count resd 1
+SECTION .bss
+    buffer resb buffer_size              ; buffer to hold file content
+    line_count resq 1                    ; 8-byte counter
 
-section .text
-    global main
-
-main:
-    ; your logic here
-    ; return from main
-    mov rax, 60
-    xor rdi, rdi
-    syscall
+SECTION .text
+    global _start
 
 _start:
-
-    ; open("names.txt", O_RDONLY)
+    ; ----------------------------
+    ; 1. Open the file (read-only)
+    ; ----------------------------
     mov rax, 2              ; syscall: sys_open
-    mov rdi, filename       ; filename
-    mov rsi, 0              ; O_RDONLY
+    mov rdi, filename       ; file path
+    mov rsi, 0              ; flags = O_RDONLY
     syscall
     cmp rax, 0
-    js _error
-    mov r12, rax            ; store file descriptor
+    js file_error           ; if rax < 0, jump to error
+    mov rdi, rax            ; save file descriptor
 
-    ; read(fd, buffer, buffer_size)
+    ; ----------------------------
+    ; 2. Read file content into buffer
+    ; ----------------------------
     mov rax, 0              ; syscall: sys_read
-    mov rdi, r12            ; fd
-    mov rsi, buffer
-    mov rdx, buffer_size
+    mov rsi, buffer         ; buffer to store content
+    mov rdx, buffer_size    ; number of bytes to read
     syscall
     cmp rax, 0
-    js _error
-    mov r13, rax            ; store bytes read
+    jle file_error          ; nothing to read or error
+    mov rcx, rax            ; store byte count (for looping)
 
-    ; Now parse the buffer
-    xor rcx, rcx            ; index in buffer
-    xor rbx, rbx            ; line length tracker
-    xor eax, eax
-    xor r8d, r8d            ; line count
+    ; ----------------------------
+    ; 3. Traverse buffer and count lines
+    ; ----------------------------
+    xor rbx, rbx            ; index = 0
+    xor rax, rax            ; line count = 0
+.next_char:
+    cmp rbx, rcx            ; check if we've reached end of buffer
+    jge .done_counting
 
-.parse_loop:
-    cmp rcx, r13
-    jge .done
+    mov dl, byte [buffer + rbx]
+    cmp dl, 10              ; check if char is '\n'
+    jne .continue_loop
 
-    mov al, [buffer + rcx]
-    inc rcx
+    ; Found a newline
+    inc rax
 
-    cmp al, 10              ; newline?
-    jne .not_newline
-
-    ; end of line found
-    inc r8d                 ; increment line count
-    xor rbx, rbx            ; reset line char count
-    jmp .parse_loop
-
-.not_newline:
+.continue_loop:
     inc rbx
-    jmp .parse_loop
+    jmp .next_char
 
-.done:
-    ; if last char wasn't newline, count last line
-    cmp rbx, 0
-    je .print
+.done_counting:
+    ; Check for case where file doesn't end with newline but has content
+    cmp rcx, 0
+    je .skip_final_add
+    mov bl, byte [buffer + rcx - 1]
+    cmp bl, 10
+    je .skip_final_add
+    inc rax                ; count last line
 
-    inc r8d
+.skip_final_add:
+    mov [line_count], rax
 
-.print:
-    mov [line_count], r8d
-
-    ; print "Line count: "
-    mov rax, 1
-    mov rdi, 1
+    ; ----------------------------
+    ; 4. Print result
+    ; ----------------------------
+    ; print message
+    mov rax, 1              ; syscall: sys_write
+    mov rdi, 1              ; stdout
     mov rsi, msg
     mov rdx, msg_len
     syscall
 
-    ; convert integer to string
-    mov eax, [line_count]
-    mov rcx, 10
-    lea rsi, [num_buf + 9]
-    mov byte [rsi], 10       ; newline
-    dec rsi
+    ; convert number to string and print
+    mov rax, [line_count]
+    call print_number
 
-.convert:
+    ; print newline
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
+
+    ; ----------------------------
+    ; 5. Exit
+    ; ----------------------------
+    mov rax, 60             ; syscall: exit
+    xor rdi, rdi            ; status 0
+    syscall
+
+; ----------------------------
+; Error handler
+; ----------------------------
+file_error:
+    mov rax, 60
+    mov rdi, 1              ; exit with code 1
+    syscall
+
+; ----------------------------
+; Helper: Print number in RAX
+; ----------------------------
+print_number:
+    ; Converts RAX to string and prints it
+    ; Uses RSI as scratch space
+    mov rcx, 10
+    lea rsi, [buffer + buffer_size - 1] ; start from end of buffer
+    mov byte [rsi], 0
+
+.print_digit:
     xor rdx, rdx
     div rcx
     add dl, '0'
-    mov [rsi], dl
     dec rsi
-    test eax, eax
-    jnz .convert
+    mov [rsi], dl
+    test rax, rax
+    jnz .print_digit
 
-    inc rsi
-    ; print number
-    mov rax, 1
-    mov rdi, 1
-    mov rdx, num_buf + 10
-    sub rdx, rsi
+    ; write number string
+    mov rax, 1      ; sys_write
+    mov rdi, 1      ; stdout
+    lea rdx, [buffer + buffer_size - 1]
+    sub rdx, rsi    ; string length
     syscall
-
-_exit:
-    mov rax, 60
-    xor rdi, rdi
-    syscall
-
-_error:
-    ; print error message
-    mov rax, 60
-    mov rdi, 1
-    syscall
+    ret
